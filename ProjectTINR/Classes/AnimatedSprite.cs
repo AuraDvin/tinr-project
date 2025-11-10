8d85adef8a5fa022d7e30392ccdf72d30d21b793
AuraDvin <edvin.garibovic@gmail.com>
2025-11-10 15:24:57 +0100

    Created AnimatedSprite (working)
    it can read json and play animations

--- ProjectTINR/Classes/AnimatedSprite.cs
+++ ProjectTINR/Classes/AnimatedSprite.cs
@@ -1,66 +1,98 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Text.Json.Nodes;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace TINR.Classes;
 
-struct AnimationFrame
-{
-    public Rectangle _rect;
-    public double duration;
+struct AnimationFrame {
+    public Rectangle Rect;
+    public double Duration;
+    public AnimationFrame(Rectangle rect, double dur) {
+        Rect = rect;
+        Duration = dur;
+    }
 }
 
-struct Animation
-{
-    public bool looping;
-    public string nextAnim;
-    public AnimationFrame[] frames;   
-    
+struct Animation {
+    public bool Looping;
+    public string NextAnim;
+    public List<AnimationFrame> Frames;
 }
 
 // TODO read json to map and play animation
-public class AnimatedSprite : Sprite
-{
-    private Dictionary<string, Animation> Spritesheet;
+public class AnimatedSprite : Sprite {
+    private readonly Dictionary<string, Animation> _spritesheet;
     private Animation _currentAnimation;
     private AnimationFrame _currentFrame;
     private int _currentFrameIdx;
     private double _lastFrameTime;
 
-    public AnimatedSprite(Game game, Rectangle rect, Vector2 position, Texture2D texture) : base(game, rect, position, texture)
-    {
+    public AnimatedSprite(Game game, Vector2 position, Texture2D texture, string jsonPath) : base(game, new Rectangle(0,0,0,0), position, texture) {
+        _spritesheet = new Dictionary<string, Animation>();
+        string jsonString = File.ReadAllText(jsonPath);
+        JsonNode animationFramesNode = JsonNode.Parse(jsonString);
+
+        foreach (var frame in animationFramesNode.AsArray()) {
+            string animationName = (string)frame["filename"];
+            if (!_spritesheet.ContainsKey(animationName)) {
+                Animation animation = new Animation();
+                animation.Frames = new List<AnimationFrame>();
+                animation.Looping = true;
+                animation.NextAnim = "TODO";
+                _spritesheet.Add(animationName, animation);
+                playAnimation(animationName);
+            }
+
+            Rectangle tmp_rect = new Rectangle(
+                (int)frame["frame"]["x"],
+                (int)frame["frame"]["y"],
+                (int)frame["frame"]["w"],
+                (int)frame["frame"]["h"]);
+
+            var durval = (double?)frame["duration"];
+            double tmp_duration;
+            if (durval == null) {
+                tmp_duration = 1000 / 12;
+            }
+            else {
+                tmp_duration = (double)durval;
+            }
+            AnimationFrame animationFrame = new AnimationFrame();
+            animationFrame.Duration = tmp_duration;
+            animationFrame.Rect = tmp_rect;
+            _spritesheet[animationName].Frames.Add(animationFrame);
+        }
+        playAnimation("idle");
     }
 
-    public void playAnimation(string animationName)
-    {
-        Animation animation = Spritesheet[animationName];
+    public void playAnimation(string animationName) {
+        Animation animation = _spritesheet[animationName];
         _currentAnimation = animation;
         _lastFrameTime = 0;
         _currentFrameIdx = 0;
     }
 
-    public override void Update(GameTime gameTime)
-    {
+    public override void Update(GameTime gameTime) {
         _lastFrameTime += gameTime.ElapsedGameTime.Milliseconds;
-        while (_lastFrameTime >= _currentAnimation.frames[_currentFrameIdx].duration)
-        {
-            _lastFrameTime -= _currentAnimation.frames[_currentFrameIdx].duration;
-            _currentFrameIdx = (_currentFrameIdx + 1) % _currentAnimation.frames.Length;
-            if (_currentFrameIdx == 0 && !_currentAnimation.looping)
-            {
-                playAnimation(_currentAnimation.nextAnim);
+        if (_lastFrameTime >= _currentAnimation.Frames[_currentFrameIdx].Duration) {
+            _lastFrameTime -= _currentAnimation.Frames[_currentFrameIdx].Duration;
+            _currentFrameIdx = (_currentFrameIdx + 1) % _currentAnimation.Frames.Count;
+            if (_currentFrameIdx == 0 && !_currentAnimation.Looping) {
+                playAnimation(_currentAnimation.NextAnim);
             }
         }
-        _currentFrame = _currentAnimation.frames[_currentFrameIdx];
+        _currentFrame = _currentAnimation.Frames[_currentFrameIdx];
+        base.SetRect(_currentFrame.Rect);
         base.Update(gameTime);
     }
 
-    public override void Draw(GameTime gameTime)
-    {
-        base.SetRect(_currentFrame._rect);
+    public override void Draw(GameTime gameTime) {
         base.Draw(gameTime);
     }
 }
--- ProjectTINR/ProjectTINR.cs
+++ ProjectTINR/ProjectTINR.cs
@@ -1,92 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
 using TINR.Classes;
 
 namespace TINR;
 
-public class ProjectTinr : Game
-{
-    private GraphicsDeviceManager _graphics;
+public class ProjectTinr : Game {
+    private readonly GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    private LinkedList<Sprite> _sprites;
+    private readonly LinkedList<Sprite> _sprites;
     private Texture2D _texture;
-    private List<Rectangle> _rects1;
-    private List<Rectangle> _rects2;
-    private int _animFrame = 0; 
-    
-    public ProjectTinr()
-    {
+    private int _animFrame = 0;
+
+    public ProjectTinr() {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
         _sprites = new LinkedList<Sprite>();
-        _rects1 = new List<Rectangle>();
-        _rects2 = new List<Rectangle>();
-        
-        _rects1.Add(new Rectangle(0, 0, 194, 194)); 
-        _rects1.Add(new Rectangle(194, 0, 194, 194)); 
-        _rects1.Add(new Rectangle(388, 0, 194, 194)); 
-        _rects1.Add(new Rectangle(582, 0, 194, 194)); 
-        _rects1.Add(new Rectangle(776, 0, 194, 194)); 
-        _rects1.Add(new Rectangle(970, 0, 194, 194)); 
-        
-        _rects2.Add(new Rectangle(1552, 970, 194, 194));
-        _rects2.Add(new Rectangle(1746, 970, 194, 194));
-        _rects2.Add(new Rectangle(1940, 970, 194, 194));
-        _rects2.Add(new Rectangle(0, 1164, 194, 194));
-        _rects2.Add(new Rectangle(194, 1164, 194, 194));
-        _rects2.Add(new Rectangle(388, 1164, 194, 194));
-        _rects2.Add(new Rectangle(582, 1164, 194, 194));
-        _rects2.Add(new Rectangle(776, 1164, 194, 194));
-        _rects2.Add(new Rectangle(970, 1164, 194, 194));
-        _rects2.Add(new Rectangle(1164, 1164, 194, 194));
-        _rects2.Add(new Rectangle(1358, 1164, 194, 194));
-        _rects2.Add(new Rectangle(1552, 1164, 194, 194));
     }
 
-    protected override void Initialize()
-    {
+    protected override void Initialize() {
         _texture = Content.Load<Texture2D>("chracters");
+
+        _sprites.AddFirst(new AnimatedSprite(this, new Vector2(420.0f, 42.0f), _texture, "Content/Spritesheet_edited.json"));
+        _sprites.AddFirst(new AnimatedSprite(this, new Vector2(GraphicsDevice.Viewport.Width * 0.5f, GraphicsDevice.Viewport.Height * 0.5f), _texture, "Content/blobi_animations.json"));
         
-        _sprites.AddFirst(new Sprite(this, _rects1[0], new Vector2(420.0f, 42.0f), _texture));
-        _sprites.AddFirst(new Sprite(this, _rects2[0], new Vector2(GraphicsDevice.Viewport.Width*0.5f, GraphicsDevice.Viewport.Height*0.5f), _texture));
-        
+       
+
         base.Initialize();
     }
 
-    protected override void LoadContent()
-    {
+    protected override void LoadContent() {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        
-
     }
 
-    protected override void Update(GameTime gameTime)
-    {
+    protected override void Update(GameTime gameTime) {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-        
-        if (gameTime.TotalGameTime.Milliseconds % 12 == 0)
-        {
-            _animFrame++;
-            _sprites.First.Value.SetRect( _rects1[_animFrame % _rects1.Count]);
-            _sprites.Last.Value.SetRect( _rects2[_animFrame % _rects2.Count]);
+
+        foreach (AnimatedSprite _sprite in _sprites) {
+            _sprite.Update(gameTime);
         }
-     
+
         base.Update(gameTime);
     }
 
-    protected override void Draw(GameTime gameTime)
-    {
+    protected override void Draw(GameTime gameTime) {
         GraphicsDevice.Clear(Color.CornflowerBlue);
         _spriteBatch.Begin();
-        foreach (var sprite in _sprites)
-        {
+        foreach (var sprite in _sprites) {
             sprite.Draw(_spriteBatch);
         }
         _spriteBatch.End();
