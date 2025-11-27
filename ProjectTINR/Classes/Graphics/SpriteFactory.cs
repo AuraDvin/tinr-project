4c3b9d57a2bd4e54e9fdd0910cfc96b543b66169
AuraDvin <edvin.garibovic@gmail.com>
2025-11-27 15:09:40 +0100

    Sprite Factory

---
+++ ProjectTINR/Classes/Graphics/SpriteFactory.cs
@@ -0,0 +1,24 @@
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using TINR.Classes;
+
+namespace ProjectTINR.Classes.Graphics;
+
+public class SpriteFactory {
+    public static Sprite CreateSprite(Game game, GameObject gameObject) {
+        AnimatedSprite animatedSprite;
+        Sprite sprite;
+        if (gameObject is Player) {
+            animatedSprite = new AnimatedSprite(game, Vector2.Zero, game.Content.Load<Texture2D>("images/characters"));
+            animatedSprite.AddAnimationFromJson("Content/Spritesheet_edited.json");
+            animatedSprite.PlayAnimation("idle");
+            return animatedSprite;
+        }
+        // if object is enemy ... 
+        sprite = new Sprite(game, new(0, 0, 194, 194), new(0, 0), game.Content.Load<Texture2D>("images/characters"));
+        return sprite;
+    }
+}
