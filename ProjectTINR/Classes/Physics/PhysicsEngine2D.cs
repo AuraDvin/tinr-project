72c8547ce877203f1703bf2fa8b1ab49b837f590
AuraDvin <edvin.garibovic@gmail.com>
2025-12-04 20:54:03 +0100

    updates

--- ProjectTINR/Classes/Physics/PhysicsEngine2D.cs
+++ ProjectTINR/Classes/Physics/PhysicsEngine2D.cs
@@ -11,9 +11,8 @@ namespace ProjectTINR.Classes.Physics;
 
 public class PhysicsEngine2D(Game game, Level level) : GameObject(game) {
     private readonly Level _level = level;
-
-    private readonly Dictionary<string, ICollisionShape> _shapes = [];
-    private readonly Dictionary<string, IPhysicsObject> _objs = [];
+    public readonly Dictionary<string, ICollisionShape> _shapes = [];
+    public readonly Dictionary<string, IStaticPhysicsObject> _objs = [];
 
     public override void Initialize() {
         base.Initialize();
@@ -22,50 +21,53 @@ public class PhysicsEngine2D(Game game, Level level) : GameObject(game) {
     public override void Update(GameTime gameTime) {
         HashSet<string> updatedObjects = [];
         foreach (GameObject obj in _level.Scene) {
-            if (obj is not IPhysicsObject) continue;
-            IPhysicsObject physicsObject = (IPhysicsObject)obj;
-
+            if (obj is not IStaticPhysicsObject) continue;
+            IStaticPhysicsObject staticPhysicsObject = (IStaticPhysicsObject)obj;
+            ICollisionShape shape;
             if (!_shapes.ContainsKey(obj.Name)) {
-                ICollisionShape shape = CollisionShapeFactory.MakeShape(physicsObject.CollisionType);
+                shape = CollisionShapeFactory.MakeShape(staticPhysicsObject.CollisionType);
                 // Why don't we add these as Components? They're not animated and simply follow the sprite's position, so 
                 // this wouldn't really make sense to do, and for debug drawing they're going to be a color not a sprite
                 // What if these shapes had to affect the scene? Like removing themselves after being picked up?
                 // check if they have a ISceneManipulator component, then pass a reference to the level?
-                if (obj is Player) {
-                    // PlayerCollisionShape playerShape = shape as PlayerCollisionShape;
-                    // shape = new PlayerCollisionShape();
-                    Rectangle rectangle = new(0, 0, 50, 100);
-                    ((RectCollisionShape)shape).Rectangle = rectangle;
-                    // shape = playerShape;
-                    // playerShape.Rectangle.Width = 30;
-                    // playerShape.Height = 50;
-                }
-                if (obj is Floor) {
-                    RectCollisionShape floorShape = shape as RectCollisionShape;
-                    Rectangle rectangle = new(0, 0, 400, 20);
-                    floorShape.Rectangle = rectangle;
-                    shape = floorShape;
-                }
+
+                shape.Position = staticPhysicsObject.Position;
                 _shapes.Add(obj.Name, shape);
-                _objs.Add(obj.Name, physicsObject);
+                _objs.Add(obj.Name, staticPhysicsObject);
                 Console.WriteLine("Added new shape");
             }
+            else {
+                shape = _shapes[obj.Name];
+            }
             updatedObjects.Add(obj.Name);
-            // Console.WriteLine(updatedObjects.Count + " updated objects in physics engine.");
+            if (shape.ShouldSimulate) {
+                shape.Position = staticPhysicsObject.Position;
 
-            if (_shapes[obj.Name].ShouldSimulate) {
-                _shapes[obj.Name].Position = physicsObject.Position;
-
-                if (_shapes[obj.Name] is IMoveComponent move) {
-                    move.Velocity = physicsObject.Velocity;
+                if (shape is not IMoveComponent) {
+                    continue;
                 }
 
-                // Update gravity 
+                // ((IMoveComponent)_shapes[obj.Name]).Velocity = physicsObject.Velocity;
                 // Console.WriteLine("Applied gravity for: " + obj.Name);
-                Vector2 objVeloc = ((IMoveComponent)obj).Velocity;
-                objVeloc.Y += (int)(10000.0 * (float)gameTime.ElapsedGameTime.TotalSeconds);
-                ((IMoveComponent)obj).Velocity = objVeloc;
+                if (staticPhysicsObject is not IMoveComponent) {
+                    continue;
+                }
 
+                IPhysicsObject physicsObject = (IPhysicsObject)staticPhysicsObject;
+                // Copy Velocity and apply gravity
+                Vector2 objVeloc = physicsObject.Velocity;
+                // if (_shapes[obj.Name] is PlayerCollisionShape playerCollisionShape && playerCollisionShape.OnFloor) {
+                //     objVeloc.Y = Math.Min(0, objVeloc.Y);
+                // }
+                // else {
+                //     // objVeloc.Y += (int)(8000.0 * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                // }
+                objVeloc.Y += (int)(10000.0 * (float)gameTime.ElapsedGameTime.TotalSeconds);
+
+                ((IMoveComponent)shape).Velocity = objVeloc;
+
+                ((IPhysicsObject)_objs[obj.Name]).Velocity = objVeloc;
+                _shapes[obj.Name] = shape;
             }
             // Add all other dynamic objects (like bullets, enemies, throwing knives)
             // Have to be logged dynanmically too, where they get a new position from the scene 
@@ -79,11 +81,10 @@ public class PhysicsEngine2D(Game game, Level level) : GameObject(game) {
                 deleteMe.Add(key);
             }
         }
-        // Console.WriteLine(deleteMe.Count + " shapes to delete in physics engine.");
         foreach (string name in deleteMe) {
             _shapes.Remove(name);
         }
-        // Console.WriteLine("Tuki sem jaaaaaa.");
+
         string[] keys = [.. _shapes.Keys];
         int length = keys.Length;
         for (int i = 0; i < length - 1; i++) {
@@ -92,46 +93,54 @@ public class PhysicsEngine2D(Game game, Level level) : GameObject(game) {
                 // If they're overlapping, move them back to the point of collision (touching)
                 // this can also be done in the algorithms class 
                 // Depending on static or non-static we may want to simulate the bounce or not 
-                RectCollisionShape shapeA = (RectCollisionShape)_shapes[keys[i]];
-                RectCollisionShape shapeB = (RectCollisionShape)_shapes[keys[j]];
-                // float dist = Vector2.DistanceSquared(shapeA.Position, shapeB.Position);
-                // if (dist >= 10000f) continue; // too far for collision anyway 
+                var shapeA = _shapes[keys[i]];
+                var shapeB = _shapes[keys[j]];
 
-                // Use CollisionAlgorithms class to check for collision based on shape types
                 bool isColliding = CollisionAlgorithms.CheckCollision(shapeA, shapeB);
-
                 if (!isColliding) {
-                    Console.WriteLine("No Collision between " + keys[i] + " and " + keys[j]);
                     continue;
                 }
-                Console.WriteLine("Collision detected between " + keys[i] + " and " + keys[j]);
 
-                // Pa bomo vidl če to dela
-                if (shapeA.ShouldSimulate) {
-                    _objs[keys[i]].Position = shapeA.Position;
-                    ((IMoveComponent)_objs[keys[i]]).Velocity = shapeA.Velocity;
+                _objs[keys[i]].Position = shapeA.Position;
+                _objs[keys[j]].Position = shapeB.Position;
+
+                if (_objs[keys[i]] is IPhysicsObject) {
+                    ((IPhysicsObject)_objs[keys[i]]).Velocity = ((IMoveComponent)shapeA).Velocity;
                 }
-                if (shapeB.ShouldSimulate) {
-                    _objs[keys[j]].Position = shapeB.Position;
-                    ((IMoveComponent)_objs[keys[j]]).Velocity = shapeB.Velocity;
+
+                if (_objs[keys[j]] is IPhysicsObject) {
+                    ((IPhysicsObject)_objs[keys[j]]).Velocity = ((IMoveComponent)shapeB).Velocity;
                 }
 
                 // Notify both shapes of the collision and resolve if both agree
-                if (true /* shapeA.OnCollision(shapeB) && shapeB.OnCollision(shapeA)*/) {
+                bool oncA = shapeA.OnCollision(shapeB);
+                bool oncB = shapeB.OnCollision(shapeA);
+
+                _objs[keys[i]].Position = shapeA.Position;
+                _objs[keys[j]].Position = shapeB.Position;
+
+                if (_objs[keys[i]] is IPhysicsObject) {
+                    ((IPhysicsObject)_objs[keys[i]]).Velocity = ((IMoveComponent)shapeA).Velocity;
+                }
+                if (_objs[keys[j]] is IPhysicsObject) {
+                    ((IPhysicsObject)_objs[keys[j]]).Velocity = ((IMoveComponent)shapeB).Velocity;
+                }
+
+                if (oncA && oncB) {
                     Console.WriteLine("Resolving Collision between " + keys[i] + " and " + keys[j]);
                     CollisionAlgorithms.ResolveCollision(shapeA, shapeB);
-                }
-                // _level.Scene.GetObjectByName(keys[i]).Position = shapeA.Position;
-                // _level.Scene.GetObjectByName(keys[j]).Position = shapeB.Position;
-                // _objs[keys[j]].Position = shapeB.Position;
+                    _objs[keys[i]].Position = shapeA.Position;
+                    _objs[keys[j]].Position = shapeB.Position;
 
-                
+                    if (_objs[keys[i]] is IPhysicsObject) {
+                        ((IPhysicsObject)_objs[keys[i]]).Velocity = ((IMoveComponent)shapeA).Velocity;
+                    }
+                    if (_objs[keys[j]] is IPhysicsObject) {
+                        ((IPhysicsObject)_objs[keys[j]]).Velocity = ((IMoveComponent)shapeB).Velocity;
+                    }
+                }
             }
         }
         base.Update(gameTime);
-    }
-
-    private void CollisionCheck(GameTime gameTime) {
-
     }
 }
