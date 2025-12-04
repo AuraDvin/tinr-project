04d66bc6a0d800683e1e66f06b446100ced8fc5e
AuraDvin <edvin.garibovic@gmail.com>
2025-12-04 12:58:10 +0100

    remove update function

--- ProjectTINR/Classes/ObjectsComponents/IMoveComponent.cs
+++ ProjectTINR/Classes/ObjectsComponents/IMoveComponent.cs
@@ -5,20 +5,6 @@ using System;
 
 namespace ProjectTINR.Classes;
 
-public interface IMoveComponent : IPositionComponent, IUpdatableGameComponent
-{
+public interface IMoveComponent : IPositionComponent, IUpdatableGameComponent {
     public Vector2 Velocity { get; set; }
-
-    public new void Update(GameTime gameTime)
-    {
-        Console.WriteLine("MoveableComp - Update called");
-        if (Velocity.LengthSquared() < 0.25f) {
-            Velocity = new(0,0);
-        }
-        if (Velocity.LengthSquared() > 640000.0f) {
-            Velocity.Normalize();
-            Velocity *= 800.0f;
-        }
-        Position += Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-    }
 }
