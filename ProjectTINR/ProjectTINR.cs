2027f5ca9ba771cc4e45a823558c8e3e18ca0158
AuraDvin <edvin.garibovic@gmail.com>
2025-12-04 13:02:50 +0100

    add debug renderer to game

--- ProjectTINR/ProjectTINR.cs
+++ ProjectTINR/ProjectTINR.cs
@@ -4,13 +4,16 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using ProjectTINR;
 using TINR.Classes.Graphics;
+using ProjectTINR.Classes.Physics;
+using ProjectTINR.Classes.Graphics;
 
 namespace TINR;
 
 public class ProjectTinr : Game {
     private readonly GraphicsDeviceManager _graphics;
-    private GameRenderer _gameRenderer;
-
+    private GameRenderer2D _gameRenderer;
+    private PhysicsEngine2D _physicsEngine;
+    private DebugRender2D _debugRender2D;
     private Level _level; 
 
     public ProjectTinr() {
@@ -21,9 +24,15 @@ public class ProjectTinr : Game {
 
     protected override void Initialize() {
         _level = LevelMaker.CreateLevel(this, LevelType.StartMenu);
+        _gameRenderer = new GameRenderer2D(this, _level);
+        _physicsEngine = new PhysicsEngine2D(this, _level); 
+        _debugRender2D = new DebugRender2D(this, _level, _physicsEngine);
+
         Components.Add(_level);
-        _gameRenderer = new GameRenderer(this, _level);
         Components.Add(_gameRenderer);
+        Components.Add(_physicsEngine);
+        Components.Add(_debugRender2D);
+
         base.Initialize();
     }
 
@@ -35,13 +44,10 @@ public class ProjectTinr : Game {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-        // _level.Update(gameTime);
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime) {
-        // Console.WriteLine("Drawing game.");
-        // _gameRenderer.Draw(gameTime);
         base.Draw(gameTime);
     }
 }
