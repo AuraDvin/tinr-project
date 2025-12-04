b95010f1bbbc65d2aaf8c4d88a6512ff27609ccc
AuraDvin <edvin.garibovic@gmail.com>
2025-12-04 12:57:29 +0100

    testing collision

--- ProjectTINR/Classes/Levels/StartMenuLevel.cs
+++ ProjectTINR/Classes/Levels/StartMenuLevel.cs
@@ -3,6 +3,8 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 
+using ProjectTINR.Classes.ObjectsComponents;
+
 namespace ProjectTINR.Classes.Levels;
 
 public class StartMenuLevel : Level {
@@ -10,8 +12,18 @@ public class StartMenuLevel : Level {
         _scene = [];
         Content = game.Content;
         IGameComponent player = new Player(game);
+        // Player player2 = new Player(game);
+        // player2.Position = new Vector2(200, -400);
+
+        Floor floor = new Floor(game);
+        _scene.Add(floor);
+        // IPhysicsObject physicsObject = new IPhysicsObject {
+        //     CollisionType = CollisionShapeType.Rectangle
+        // };
         _scene.Add(player);
+        // _scene.Add(player2);
         game.Components.Add(player);
+        // game.Components.Add(player2);
         Console.WriteLine("Start Menu Level initialized.");
     }
 
