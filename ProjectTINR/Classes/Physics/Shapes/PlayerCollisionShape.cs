274506d847aabc1c7bd610c37bc710aafac61ce2
aura <edvin.garibovic@gmail.com>
2025-12-06 13:10:19 +0100

    fixed collision with walls

--- ProjectTINR/Classes/Physics/Shapes/PlayerCollisionShape.cs
+++ ProjectTINR/Classes/Physics/Shapes/PlayerCollisionShape.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 
+using Microsoft.VisualBasic;
 using Microsoft.Xna.Framework;
 
 namespace ProjectTINR.Classes.Physics.Shapes;
@@ -26,10 +28,39 @@ public class PlayerCollisionShape : RectCollisionShape {
         // Static objects should not resolve collisions
         // Non-static objects have to handle collision with a static object themselves
         // This should be Floor collision class rather than the Class for the floor object
-        if (other is FloorCollisionShape) {
-        //     Console.WriteLine("Player collided with Floor. Setting vertical velocity to 0 if positive.");
-            Velocity = new(Velocity.X, Math.Min(Velocity.Y, 0)); 
-            _onFloor = true;
+        if (other is FloorCollisionShape floor) {
+            Rectangle rect = floor.Rectangle;
+            int top = 0, bottom = 1, left = 2, right = 3;
+            var distances =  new List<float> {
+                Math.Abs(_rectangle.Bottom - rect.Top), // Top
+                Math.Abs(_rectangle.Top - rect.Bottom), // Bottom
+                Math.Abs(_rectangle.Right - rect.Left), // Left
+                Math.Abs(_rectangle.Left - rect.Right)  // Right
+            };
+            int min = -1;
+            float minDistance = float.MaxValue;
+            for (int i = 0; i < distances.Count; i++) {
+                if (minDistance > distances[i]) {
+                    minDistance = distances[i];
+                    min = i;
+                }
+                
+            }
+
+            if (min == top) {
+                // Console.WriteLine("Player is on top of the floor!");
+                Velocity = new(Velocity.X, Math.Min(Velocity.Y, 0)); 
+                _onFloor = true;
+            } else if (min == bottom) {
+                // Console.WriteLine("Player is under the floor!");
+                Velocity = new(Velocity.X, Math.Max(Velocity.Y, 0)); 
+            } else if (min == left) {
+                // Console.WriteLine("Player is to the left of the floor!");
+                Velocity = new(Math.Min(Velocity.X, 0), Velocity.Y);
+            } else if (min == right) {
+                // Console.WriteLine("Player is to the right of the floor!");
+                Velocity = new(Math.Max(Velocity.X, 0), Velocity.Y);
+            }
             return false;
         }
 
