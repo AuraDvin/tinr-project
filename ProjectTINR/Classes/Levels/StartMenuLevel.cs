c9ca51ce889f2447db5d2d7b525e147f9d509f3d
aura <edvin.garibovic@gmail.com>
2025-12-06 17:09:45 +0100

    Move to Initialize()

--- ProjectTINR/Classes/Levels/StartMenuLevel.cs
+++ ProjectTINR/Classes/Levels/StartMenuLevel.cs
@@ -8,32 +8,38 @@ using ProjectTINR.Classes.ObjectsComponents;
 namespace ProjectTINR.Classes.Levels;
 
 public class StartMenuLevel : Level {
+    private Vector2 _playerSpawnPosition = new(0f,0f);
     public StartMenuLevel(Game game) : base(game) {
         _scene = [];
-        Content = game.Content;
-
-        Floor floor = new Floor(game, new(0, 400));
-        _scene.Add(floor);
-
-        Floor floor2 = new Floor(game, new(500, 200));
-        _scene.Add(floor2);
-
-        Player player = new Player(game);
-        _scene.Add(player);
-        game.Components.Add(player);
-
-        player.Position = new Vector2(0, 0);
-
-        Console.WriteLine("Start Menu Level initialized.");
     }
 
     public ContentManager Content { get; }
 
     public override void Initialize() {
+        Floor floor = new(Game, new(0, 400));
+        Floor floor2 = new(Game, new(500, 200));
+        Player player = new(Game) {
+            Position = _playerSpawnPosition
+        };
+
+        _scene.Add(floor2);
+        _scene.Add(floor);
+        _scene.Add(player);
+
+        foreach (IGameComponent obj in _scene) {
+            Game.Components.Add(obj);
+        }
+        Console.WriteLine("Start Menu Level initialized.");
         base.Initialize();
     }
 
     public override void Update(GameTime gameTime) {
         base.Update(gameTime);
+    }
+
+    public override void Reset() {
+        Player player = _scene.FindByType<Player>();
+        player.Position = _playerSpawnPosition;
+        base.Reset();
     }
 }
