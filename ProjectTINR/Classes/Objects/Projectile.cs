c14195b4f4f1947e5e7c8beed911c4bd50f628c1
aura <edvin.garibovic@gmail.com>
2025-12-06 22:02:50 +0100

    Projectile object

---
+++ ProjectTINR/Classes/Objects/Projectile.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Reflection.Metadata;
+
+using Microsoft.Xna.Framework;
+
+using ProjectTINR.Classes.ObjectsComponents;
+using ProjectTINR.Classes.Physics.Shapes;
+
+namespace ProjectTINR.Classes.Objects;
+
+public class Projectile : GameObject, IPhysicsObject {
+    public Projectile(ProjectileCollisionShape shape, Game game)  : base(game){
+        ProjectileShape = shape;
+    }
+    public ProjectileCollisionShape ProjectileShape {get; set; }
+    public CollisionShapeType CollisionType { get => CollisionShapeType.Projectile; set{} }
+    public Vector2 Velocity { get; set; }
+    public Vector2 Position { get; set; }    
+}
