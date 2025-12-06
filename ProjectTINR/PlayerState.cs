0e0346079e6464458878bfffd28170495be1961b
aura <edvin.garibovic@gmail.com>
2025-12-06 12:54:35 +0100

    planning on checking multiple states at once with 1 variable

--- ProjectTINR/PlayerState.cs
+++ ProjectTINR/PlayerState.cs
@@ -2,15 +2,16 @@ namespace ProjectTINR;
 
 public enum PlayerState
 {
-    None,
-    Idling, 
-    Moving,
-    Jumping,
-    Falling,
-    TookDmg,
-    Frozen, 
-    Shooting,
+    None = 0,
+    Idling = 1, 
+    Moving = 2, // 1 << 1
+    Jumping = 4, // 1 << 2
+    Falling = 8, // 1 << 3
+    TookDmg = 16, // 1 << 4
+    Frozen = 32, // 1 << 5
+    Shooting = 64, // 1 << 6
 }
+
 public enum PlayerDirection {
     Left,
     Right
