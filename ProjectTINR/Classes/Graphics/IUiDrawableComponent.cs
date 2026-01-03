f2c027dcf4bc844d005592e1f9f21d2655933c4f
aura <edvin.garibovic@gmail.com>
2026-01-03 18:23:02 +0100

    ui interface

---
+++ ProjectTINR/Classes/Graphics/IUiDrawableComponent.cs
@@ -0,0 +1,18 @@
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectTINR.Classes.Graphics;
+
+public interface IUiDrawableComponent {
+    // Gives you if the Ui Element has a Label
+    public string String {get; set;}
+    // Gives you icon/texture if existing
+    public Texture2D Texture { get; set; }
+    public Vector2 TextPosition {get; set;}
+    public Vector2 TexturePosition {get; set;}
+    // If it should be drawn or not
+    public bool Visible { get; set; }
+
+}
