95f4a08f08b6b2672e096571cb5b308b6073837d
AuraDvin <edvin.garibovic@gmail.com>
2026-01-05 11:53:39 +0100

    camera

---
+++ ProjectTINR/Classes/Graphics/Camera2D.cs
@@ -0,0 +1,15 @@
+using System;
+
+using Microsoft.Xna.Framework;
+
+namespace ProjectTINR.Classes.Graphics;
+
+public class Camera2D {
+    // Camera world position (top left corner)
+    public Vector2 Position { get; set; } = Vector2.Zero;
+    public float Zoom { get; set; } = 1f;
+    public Camera2D() {
+    }
+
+
+}
