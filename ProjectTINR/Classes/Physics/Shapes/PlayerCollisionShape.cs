304985f3501d307a20884abddc94e8c69ca2faf5
AuraDvin <edvin.garibovic@gmail.com>
2026-01-09 13:50:03 +0100

    Better gravity + jumping + walljump

--- ProjectTINR/Classes/Physics/Shapes/PlayerCollisionShape.cs
+++ ProjectTINR/Classes/Physics/Shapes/PlayerCollisionShape.cs
@@ -13,29 +13,34 @@ namespace ProjectTINR.Classes.Physics.Shapes;
 
 public class PlayerCollisionShape : RectCollisionShape, ISceneManipulator {
     protected float _playerAccel = 200f;
-    protected float _playerJumpForce = 100f;
-    // protected float _playerGravity = 10000f;
+    protected float _playerJumpForce = 600f;
     protected float _playerGravity = 1000f;
     protected float _playerFriction = 8f;
     public override Vector2 Offset { get => new(50, 0); }
-    public override bool OnFloor { get; set; } = false;
-    protected float _msSinceLastJump = 0f;
-    protected readonly float _jumpTimerBeforeApplyingGravity = 2;
-
+    private int _wallJumpCount = 0; 
+    private readonly int _maxWallJumps = 1;
+    protected readonly float _jumpTimerBeforeApplyingGravity = 0.005f;
+    protected float _msSinceLastJump;
     protected bool _tookDmg = false;
     protected float _lastTookDmg = 0f;
     protected float _immuneFramesMS = 33f;
 
+    public PlayerCollisionShape() : base(false){
+        // Apply gravity right away 
+        _msSinceLastJump = _jumpTimerBeforeApplyingGravity; 
+        _rectangle = new Rectangle(0, 0, 194 / 2, 194);
+    }
+
     public override void Update(GameTime gameTime) {
         Player player = Owner as Player ?? throw new Exception("Where is the player reference?");
         float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
         Vector2 objVeloc = Velocity;
         switch (player.State) {
             case PlayerState.Idling:
                 objVeloc = Vector2.Lerp(objVeloc, new(0, objVeloc.Y), _playerFriction * dt);
                 break;
             case PlayerState.Moving:
-                // Console.WriteLine($"[PlayerCollisionShape] Player direction: {player.Direction}");
                 int sign;
                 switch (player.Direction) {
                     case PlayerDirection.Left:
@@ -58,15 +63,18 @@ public class PlayerCollisionShape : RectCollisionShape, ISceneManipulator {
                 break;
             case PlayerState.Jumping:
                 if (WasOnFloor) {
-                    // Console.WriteLine("Player is jumping from floor.");
-                    // Console.WriteLine($"Velocity before: {objVeloc}");
                     objVeloc.Y = -_playerJumpForce;
-                    // objVeloc.X = player.Direction == PlayerDirection.Left ? -_playerJumpForce : _playerJumpForce;
-                    // Console.WriteLine($"Velocity after: {objVeloc}");
                     _msSinceLastJump = 0f;
                 }
-                else {
-                    // Console.WriteLine("Player is in the air, cannot jump again.");
+                else if (WasOnWall && _wallJumpCount < _maxWallJumps) {
+                    objVeloc.Y = -_playerJumpForce;
+                    if (player.Direction == PlayerDirection.Left) {
+                        objVeloc.X = _playerJumpForce / 2;
+                    }
+                    else if (player.Direction == PlayerDirection.Right) {
+                        objVeloc.X = -_playerJumpForce / 2;
+                    }
+                    _wallJumpCount++;
                 }
                 break;
             case PlayerState.Falling:
@@ -81,11 +89,13 @@ public class PlayerCollisionShape : RectCollisionShape, ISceneManipulator {
 
         if (WasOnFloor) {
             objVeloc.Y = Math.Min(objVeloc.Y, 0);
+            _wallJumpCount = 0;
         }
 
         _msSinceLastJump += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        // Apply gravity if enough time has passed
         if (_msSinceLastJump >= _jumpTimerBeforeApplyingGravity) {
-            // Apply gravity 
             objVeloc.Y += _playerGravity * dt;
         }
 
@@ -101,14 +111,8 @@ public class PlayerCollisionShape : RectCollisionShape, ISceneManipulator {
                 _lastTookDmg -= dt;
             }
         }
-
-        // Console.WriteLine($"Ok so I'm Player with this velocity now: {Velocity}");
-        // Console.WriteLine($"Ok so I'm Player with this Position now: {Position}");
     }
 
-    public PlayerCollisionShape() : base(false) {
-        _rectangle = new Rectangle(0, 0, 194 / 2, 194);
-    }
     public override bool OnCollision(ICollisionShape other) {
 
         if (other is EnemyProjectileCollisionShape) {
@@ -123,6 +127,7 @@ public class PlayerCollisionShape : RectCollisionShape, ISceneManipulator {
             float overlapY = Math.Min(floorRect.Bottom, _rectangle.Bottom) - Math.Max(floorRect.Top, _rectangle.Top);
 
             if (overlapX < overlapY) {
+                OnWall = true;
                 if (_rectangle.Center.X < floorRect.Center.X) {
                     // Player to the left of the floor (wall)
                     Console.WriteLine("Player is to the left of the floor!");
