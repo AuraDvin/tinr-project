7dea3c778684bf04838fe7a002a5dc57d08f54c4
AuraDvin <edvin.garibovic@gmail.com>
2026-01-13 00:13:41 +0100

    simplified interface for ui

--- ProjectTINR/Classes/UI/IUiDrawableComponent.cs
+++ ProjectTINR/Classes/UI/IUiDrawableComponent.cs
@@ -3,16 +3,18 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
+using ProjectTINR.Classes.ObjectsComponents;
+
 namespace ProjectTINR.Classes.UI;
 
-public interface IUiDrawableComponent {
-    // Gives you if the Ui Element has a Label
-    public string String {get; set;}
-    // Gives you icon/texture if existing
-    public Texture2D Texture { get; set; }
-    public Vector2 TextPosition {get; set;}
-    public Vector2 TexturePosition {get; set;}
-    // If it should be drawn or not
+public interface IUiDrawableComponent : IPositionComponent {
     public bool Visible { get; set; }
-
+    public bool Enabled { get; set; }
+    public bool HasString { get; }
+    public bool HasTexture { get; }
+    public string String { get; set; }
+    public string TextureName { get; set; }
+    public Vector2 TextPosition { get; set; }
+    public Vector2 TexturePosition { get; set; }
+    public Rectangle TextureRect { get; set; }
 }
