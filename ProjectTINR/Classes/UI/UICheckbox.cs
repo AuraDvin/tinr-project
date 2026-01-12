8e592b6a6a2bfc9158f8c811857c9ce316d109ed
AuraDvin <edvin.garibovic@gmail.com>
2026-01-13 00:12:24 +0100

    switch state clickable label

--- ProjectTINR/Classes/UI/UICheckbox.cs
+++ ProjectTINR/Classes/UI/UICheckbox.cs
@@ -1,24 +1,28 @@
+using System;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ProjectTINR.Classes.ObjectsComponents;
 
 namespace ProjectTINR.Classes.UI;
 
-public class UICheckbox : GameObject, IUiDrawableComponent {
-    public string Label { get; set; } = "";
+public class UICheckbox : UIButton {
     public bool Checked { get; set; } = false;
-    private Vector2 _pos = Vector2.Zero;
 
-    public UICheckbox(Game game) : base(game) { }
-
-    // String to be drawn by the UiRenderer2D
-    public string String { get => $"{Label} [{(Checked ? 'X' : ' ')}]"; set { } }
-    public Texture2D Texture { get; set; } = null;
-    public Vector2 TextPosition { get => _pos; set => _pos = value; }
-    public Vector2 TexturePosition { get; set; } = Vector2.Zero;
-    public bool Visible { get; set; } = true;
+    public UICheckbox(Game game, string str, string textureName) : base(game, str, textureName) {
+    }
 
     public void Toggle() {
         Checked = !Checked;
+    }
+    
+    public override void OnClick() {
+        Toggle();
+        base.OnClick();
+    }
+
+    public override void OnClick(string message, object args) {
+        Toggle();
+        base.OnClick(message, args);
     }
 }
