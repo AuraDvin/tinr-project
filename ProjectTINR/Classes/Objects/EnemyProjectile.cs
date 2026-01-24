9111909e1810f769f67eef4562c8cfd5e7f8278e
aura <edvin.garibovic@gmail.com>
2026-01-24 15:01:44 +0100

    Enemy and Player projectiles make oneshot sounds

--- ProjectTINR/Classes/Objects/EnemyProjectile.cs
+++ ProjectTINR/Classes/Objects/EnemyProjectile.cs
@@ -1,7 +1,9 @@
 using Microsoft.Xna.Framework;
 
+using ProjectTINR.Classes.ObjectsComponents;
+
 namespace ProjectTINR.Classes.Objects;
 
-public class EnemyProjectile(Game game) : Projectile(game) {
+public class EnemyProjectile(Game game) : Projectile(game), ISoundPlayer {
     public override CollisionShapeType CollisionType { get; set; } = CollisionShapeType.EnemyProjectile;
 }
--- ProjectTINR/Classes/Objects/PlayerProjectile.cs
+++ ProjectTINR/Classes/Objects/PlayerProjectile.cs
@@ -1,7 +1,9 @@
 using Microsoft.Xna.Framework;
 
+using ProjectTINR.Classes.ObjectsComponents;
+
 namespace ProjectTINR.Classes.Objects;
 
-public class PlayerProjectile(Game game) : Projectile(game){
+public class PlayerProjectile(Game game) : Projectile(game), ISoundPlayer{
     public override CollisionShapeType CollisionType { get => CollisionShapeType.PlayerProjectile; }
 }
