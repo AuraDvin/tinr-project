d05d7939e24710ff7e8d9a25557274ee5dfee81e
aura <edvin.garibovic@gmail.com>
2026-01-24 15:01:33 +0100

    Projectile sfx in factory

--- ProjectTINR/Classes/Sound/SoundControllerFactory.cs
+++ ProjectTINR/Classes/Sound/SoundControllerFactory.cs
@@ -12,7 +12,13 @@ public class SoundControllerFactory {
         if (gameObject is Player player) {
             return new PlayerSoundController(game, player);
         }
-
+        
+        if (gameObject is PlayerProjectile) {
+            return new ProjectileSoundController(game, gameObject, "audio/sounds/flying-knife");
+        } else if (gameObject is Projectile) {
+            return new ProjectileSoundController(game, gameObject, "audio/sounds/jump");
+        }
+        
         return null;
     }
 }
