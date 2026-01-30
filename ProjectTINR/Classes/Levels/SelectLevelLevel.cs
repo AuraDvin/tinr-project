61a4153eac9558d921e000530b576a8ccb3b5f67
aura <edvin.garibovic@gmail.com>
2026-01-30 18:41:41 +0100

    Moving and pressing level buttons

--- ProjectTINR/Classes/Levels/SelectLevelLevel.cs
+++ ProjectTINR/Classes/Levels/SelectLevelLevel.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+
 using ProjectTINR.Classes.UI;
 
 namespace ProjectTINR.Classes.Levels;
 
 class LevelObserver : Observer {
-    ProjectTinr _game; 
+    ProjectTinr _game;
 
     public LevelObserver(ProjectTinr game) {
         _game = game;
@@ -28,7 +29,7 @@ class LevelObserver : Observer {
         Console.WriteLine($"Level {levelIndx} selected.");
         // Tell game to load the right level layout
 
-        _game.SwitchLevel(LevelType.MainLevel);
+        // _game.SwitchLevel(LevelType.MainLevel);
     }
 }
 
@@ -36,13 +37,14 @@ class CustomButton : UIButton {
     private int _levelIndex;
     private LevelObserver _observer;
 
-    public CustomButton(Game game, int levelIndex, LevelObserver observer) :   base(game, levelIndex.ToString(), "") {
+    public CustomButton(Game game, int levelIndex, LevelObserver observer) : base(game, levelIndex.ToString(), "") {
         _levelIndex = levelIndex;
+        _observer = observer;
         AddObserver(observer);
     }
 
     public override void OnClick() {
-        base.OnClick();
+        // base.OnClick();
         _observer.OnNotify(_levelIndex);
     }
 }
@@ -52,10 +54,12 @@ public class SelectLevelLevel : Level {
     private float _inputDelay = 0.2f;
     private float _timeSinceLastInput = 0f;
     private UIHorizontalList _levelRow1, _levelRow2;
-    private int _levelSelected = 0; 
+    private UIVerticalList _levelColumn;
+    private int _levelSelected = 0;
+    private int _lastLevelSelected = 0;
     private int _rowSelected = 0;
     private int _numberOfLevels = 10;
-    private KeyboardState _lastKB;
+    private KeyboardState _lastKbState;
     LevelObserver _levelObserver;
     public SelectLevelLevel(Game game, int numberOfLevels) : base(game) {
         _scene = new Scene();
@@ -65,112 +69,105 @@ public class SelectLevelLevel : Level {
         _levelObserver = new LevelObserver(game as ProjectTinr);
 
         // Initialize level rows
-        _levelRow1 = new UIHorizontalList(game);
-        _levelRow2 = new UIHorizontalList(game);
+        _levelRow1 = new UIHorizontalList(game) {
+            Visible = true
+        };
+        _levelRow2 = new UIHorizontalList(game) {
+            Visible = true
+        };
 
         // Add level buttons to rows
         for (int i = 0; i < numberOfLevels; i++) {
             // todo: if level is locked, create a locked button instead
-            CustomButton button = new(game, i, _levelObserver) {
+            CustomButton button = new(game, i + 1, _levelObserver) {
                 String = $"{i + 1}",
                 Visible = true,
             };
 
             if (i < numberOfLevels / 2) {
                 _levelRow1.Children.Add(button);
-            } else {
+            }
+            else {
                 _levelRow2.Children.Add(button);
             }
         }
 
-        // Position the rows
-        Vector2 row1Position = new Vector2(
-            Game.GraphicsDevice.Viewport.Width / 2 - 100,
-            Game.GraphicsDevice.Viewport.Height / 2 - 50);
-        Vector2 row2Position = new Vector2(
-            Game.GraphicsDevice.Viewport.Width / 2 - 100,
-            Game.GraphicsDevice.Viewport.Height / 2 + 50);
+        _levelColumn = new UIVerticalList(game) {
+            Children = [_levelRow1, _levelRow2],
+            Position = new(0, 0)
+        };
 
-        _levelRow1.Position = row1Position;
-        _levelRow2.Position = row2Position;
-        _uiScene.Add(_levelRow1);
-        _uiScene.Add(_levelRow2);
+        _uiScene.Add(_levelColumn);
 
         Console.WriteLine("SelectLevelLevel initialized with ", numberOfLevels, " levels.");
-        _levelRow1.Visible = true;
-        _levelRow2.Visible = true;
     }
     public override void Update(GameTime gameTime) {
         base.Update(gameTime);
-        // KeyboardState kb = Keyboard.GetState();
-        // _timeSinceLastInput += (float)gameTime.ElapsedGameTime.TotalSeconds;
-        // if (_timeSinceLastInput < _inputDelay) {
-        //     return;
-        // }
+        KeyboardState kbState = Keyboard.GetState();
+        _timeSinceLastInput += (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-        // if (Pressed(kb, Keys.Escape)) {
-        //     // Handle escape key to go back to main menu
-        //     _levelObserver.Notify();
-        //     _timeSinceLastInput = 0f;
-        // }
+        if (_timeSinceLastInput < _inputDelay) {
+            return;
+        }
 
-        // if (Released(kb, Keys.Up) || Released(kb, Keys.W)){
-        //     _rowSelected = (_rowSelected + 1) % 2;
-        //     if (_rowSelected == 0) {
-        //         _levelRow2.Enabled = false;
-        //         _levelRow1.Enabled = true;
-        //     } else {
-        //         _levelRow1.Enabled = false;
-        //         _levelRow2.Enabled = true;
-        //     }
-        //     _timeSinceLastInput = 0f;
-        // }
+        if (Pressed(kbState, Keys.Escape)) {
+            _levelObserver.Notify();
+            _timeSinceLastInput = 0f;
+        }
 
-        // if (Released(kb, Keys.Down) || Released(kb, Keys.S)){
-        //     _rowSelected = _rowSelected == 0 ? 1 : 0;
-        //     if (_rowSelected == 0) {
-        //         _levelRow2.Enabled = false;
-        //         _levelRow1.Enabled = true;
-        //     } else {
-        //         _levelRow1.Enabled = false;
-        //         _levelRow2.Enabled = true;
-        //     }
-        //     _timeSinceLastInput = 0f;
-        // }
+        if (Released(kbState, Keys.Enter) || Released(kbState, Keys.Space)){
+            ((_levelColumn.Children[_rowSelected] as ComplexUIElement).Children[_levelSelected] as CustomButton).OnClick();
+            _timeSinceLastInput = 0f;
+        }
 
-        // if (Released(kb, Keys.Enter) || Released(kb, Keys.Space)){
-        //     // Load the selected level
-        //     if (_rowSelected == 0) {
-        //         _levelRow1.ActivateSelected();
-        //         // Console.WriteLine($"Loading Level {levelIndex + 1} from Row 1");
-        //     } else {
-        //         _levelRow2.ActivateSelected();
-        //         // int levelIndex = _levelRow2.GetSelectedIndex() + _numberOfLevels / 2;
-        //         // Console.WriteLine($"Loading Level {levelIndex + 1} from Row 2");
-        //     }
-        //     _timeSinceLastInput = 0f;
-        // }
+        if (kbState.IsKeyDown(Keys.Up) || kbState.IsKeyDown(Keys.Down)){
+            _rowSelected = (_rowSelected + 1) % 2;
+            if (_rowSelected == 0) {
+                foreach(SimpleUIElement s in _levelRow2.Children) {
+                    s.Selected = false;
+                }
+            } else {
+                foreach(SimpleUIElement s in _levelRow1.Children) {
+                    s.Selected = false;
+                }
+            }
+            _timeSinceLastInput = 0f;
+        }
 
-        // if (kb.IsKeyDown(Keys.Left)) {
-        //     _levelRow1.SelectPrevious();
-        //     _levelRow2.SelectPrevious();
-        //     _timeSinceLastInput = 0f;
-        // }
+        if (kbState.IsKeyDown(Keys.Left)) {
+            _levelSelected -= 1;
+            if (_levelSelected < 0) {
+                _levelSelected = CurrentRowCount - 1;
+            }
+            _timeSinceLastInput = 0f;
+        }
 
-        // if (kb.IsKeyDown(Keys.Right)) {
-        //     _levelRow2.SelectNext();
-        //     _levelRow1.SelectNext();
-        //     _timeSinceLastInput = 0f;
-        // }
+        if (kbState.IsKeyDown(Keys.Right)) {
+            _levelSelected += 1; 
+            _levelSelected %= CurrentRowCount;
+            _timeSinceLastInput = 0f;
+        }
 
+        UpdateSelected();
 
-        // _lastKB = kb;
+        _lastLevelSelected = _levelSelected;
+        _lastKbState = kbState;
     }
-    
+
+    private int CurrentRowCount  {
+        get => (_levelColumn.Children[_rowSelected] as ComplexUIElement).Children.Count;
+    }
+
+    private void UpdateSelected() {
+        UIHorizontalList row = _levelColumn.Children[_rowSelected] as UIHorizontalList;        
+        (row.Children[_lastLevelSelected] as SimpleUIElement).Selected = false;
+        (row.Children[_levelSelected] as SimpleUIElement).Selected = true;
+    }
+
     private bool Pressed(KeyboardState currrentKB, Keys key) {
-        return currrentKB.IsKeyDown(key) && !_lastKB.IsKeyDown(key);
+        return currrentKB.IsKeyDown(key) && !_lastKbState.IsKeyDown(key);
     }
     private bool Released(KeyboardState currrentKB, Keys key) {
-        return !currrentKB.IsKeyDown(key) && _lastKB.IsKeyDown(key);
+        return !currrentKB.IsKeyDown(key) && _lastKbState.IsKeyDown(key);
     }
 }
