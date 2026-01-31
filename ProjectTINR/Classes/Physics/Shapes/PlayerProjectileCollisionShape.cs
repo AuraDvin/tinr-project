bca005ae41da2ae6e8abee2f0a85a9c3a43e55c2
aura <edvin.garibovic@gmail.com>
2026-01-31 20:14:34 +0100

    use scale

--- ProjectTINR/Classes/Physics/Shapes/PlayerProjectileCollisionShape.cs
+++ ProjectTINR/Classes/Physics/Shapes/PlayerProjectileCollisionShape.cs
@@ -1,9 +1,21 @@
+using System;
+using System.Buffers;
+
 using Microsoft.Xna.Framework;
 
+using ProjectTINR.Classes.Objects;
 using ProjectTINR.Classes.ObjectsComponents;
 
 namespace ProjectTINR.Classes.Physics.Shapes;
 
-public class PlayerProjectileCollisionShape(Vector2 startingPosition, int direction, Game game)
-    : ProjectileCollisionShape(startingPosition, direction, game) {
+public class PlayerProjectileCollisionShape
+    : ProjectileCollisionShape {
+    public PlayerProjectileCollisionShape(Vector2 startingPosition, int direction, float scale, Game game) : base(startingPosition, direction, game) {
+        // if (Owner is PlayerProjectile pp) {
+        //     Console.WriteLine($"Radius {_radius} {Offset} owner: {Owner}");
+        // }
+        _radius *= scale;
+        Offset *= scale;
+        Console.WriteLine($"Radius {_radius} {Offset}");
+    }
 }
