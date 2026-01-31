6fa73d12415eb2a62cfc10eafec3806b85117036
aura <edvin.garibovic@gmail.com>
2026-01-31 16:13:42 +0100

    set offset and size

--- ProjectTINR/Classes/Physics/Shapes/PickupCollisionShape.cs
+++ ProjectTINR/Classes/Physics/Shapes/PickupCollisionShape.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Reflection.Metadata.Ecma335;
 
+using Microsoft.Xna.Framework;
+
 using ProjectTINR.Classes.Objects;
 using ProjectTINR.Classes.ObjectsComponents;
 
 namespace ProjectTINR.Classes.Physics.Shapes;
 
 public class PickupCollisionShape : CircleCollisionShape {
-    public PickupCollisionShape() : base(true, 2f) {
+    public override Vector2 Offset { get => new(62, 64); set { } }
+    public PickupCollisionShape() : base(true, 55f) {
     }
     public override bool OnCollision(ICollisionShape other) {
         if (other is PlayerCollisionShape) {
