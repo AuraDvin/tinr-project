92d914081430da1e26de2741c7b3117e9f32f03c
aura <edvin.garibovic@gmail.com>
2026-01-31 20:13:18 +0100

    enemies health

--- ProjectTINR/Classes/NPCs/Enemy.cs
+++ ProjectTINR/Classes/NPCs/Enemy.cs
@@ -12,13 +12,18 @@ public abstract class Enemy(Game game) : GameObject(game), IPhysicsObject, ICont
     public virtual CollisionShapeType CollisionType { get; set; } = CollisionShapeType.StaticRectangle;
     public bool SeesPlayer = false;
     protected float _seeingDistance = 450f;
-
+    protected float _health = 3;
+    public float Health { get => _health; set => _health = value; }
     public virtual Vector2 Velocity { get; set; }
     public virtual Vector2 Position { get; set; }
     public virtual ControllerType ControllerType => ControllerType.AiController;
     public Scene Scene { get; set; } = null;
 
     public override void Update(GameTime gameTime) {
+        if (Health <= 0) {
+            Scene.Remove(this);
+        }
+
         float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
         Position += Velocity * dt;
 
