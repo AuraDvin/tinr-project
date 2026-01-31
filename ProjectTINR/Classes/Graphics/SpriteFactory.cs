b08d19ee65179e18a080ea9efde0bcae8a51d44d
aura <edvin.garibovic@gmail.com>
2026-01-31 20:13:42 +0100

    projectile sprites

--- ProjectTINR/Classes/Graphics/SpriteFactory.cs
+++ ProjectTINR/Classes/Graphics/SpriteFactory.cs
@@ -17,8 +17,18 @@ public class SpriteFactory {
     private static readonly string TestFloorPath = "images/test";
     private static readonly Rectangle FloorTextureRect = new(0, 0, 1000, 400);
     private static readonly Rectangle StationaryEnemyTextureRect = new(0, 0, 194, 194);
+    private static readonly Rectangle FlyingEnemyTextureRect = new(1358, 970, 194, 194);
+    private static Texture2D enemyProjectile;
+    private static Texture2D playerProjectile;
     private static Texture2D pickupTexture;
     public static Sprite CreateSprite(Game game, GameObject gameObject) {
+        if (enemyProjectile == null) {
+            enemyProjectile = game.Content.Load<Texture2D>("images/enemyprojectile");
+        }
+        if (playerProjectile == null) {
+            playerProjectile = game.Content.Load<Texture2D>("images/playerprojectile");
+        }
+
         AnimatedSprite animatedSprite;
         Sprite sprite;
         if (gameObject is Player) {
@@ -29,21 +39,36 @@ public class SpriteFactory {
             return animatedSprite;
         }
 
+        if (gameObject is Projectile pp) {
+            bool right = pp.FacingRight;
+            if (gameObject is PlayerProjectile p) {
+                return new Sprite(game, new Rectangle(0, 0, 256, 256), p.Position, playerProjectile) {
+                    SpriteEffects = !right ? SpriteEffects.FlipHorizontally : SpriteEffects.None
+                };
+            } else if (gameObject is EnemyProjectile ep) {
+                return new Sprite(game, new Rectangle(0, 0, 256, 256), ep.Position, enemyProjectile) {
+                    SpriteEffects = !right ? SpriteEffects.FlipHorizontally : SpriteEffects.None
+                };
+            }
+        }
+
         if (gameObject is Floor floor) {
             // Console.WriteLine("Creating floor sprite");
             sprite = new Sprite(game, floor.BoundingBox, Vector2.Zero, game.Content.Load<Texture2D>(TestFloorPath));
             return sprite;
         }
 
-        if (gameObject is StationaryEnemy) {
-            sprite = new Sprite(game, StationaryEnemyTextureRect, Vector2.Zero, game.Content.Load<Texture2D>(CharactersPath));
+        if (gameObject is StationaryEnemy se) {
+            // sprite = new Sprite(game, StationaryEnemyTextureRect, Vector2.Zero, game.Content.Load<Texture2D>(CharactersPath));
+            animatedSprite = new AnimatedSprite(game, se.Position, game.Content.Load<Texture2D>(CharactersPath));
+            animatedSprite.AddAnimationFromJson("Content/blobi_animations.json");
+            animatedSprite.PlayAnimation("idle");
 
-            return sprite;
+            return animatedSprite;
         }
 
         if (gameObject is FlyingEnemy) {
-            sprite = new Sprite(game, StationaryEnemyTextureRect, Vector2.Zero, game.Content.Load<Texture2D>(CharactersPath));
-
+            sprite = new Sprite(game, FlyingEnemyTextureRect, Vector2.Zero, game.Content.Load<Texture2D>(CharactersPath));
             return sprite;
         }
 
@@ -55,16 +80,16 @@ public class SpriteFactory {
             Rectangle textureRect;
             switch (po.Type) {
                 case PickupType.HEAL:
-                    textureRect = new (0, 0, 128, 128);
+                    textureRect = new(0, 0, 128, 128);
                     break;
                 case PickupType.BIGGER_PROJECTILE:
                     textureRect = new(128, 0, 128, 128);
                     break;
                 case PickupType.SHOOT_SPEED:
-                    textureRect = new (0, 128, 128, 128);   
+                    textureRect = new(0, 128, 128, 128);
                     break;
                 case PickupType.CHECKPOINT:
-                    textureRect = new (128, 128, 128, 128);
+                    textureRect = new(128, 128, 128, 128);
                     break;
                 default:
                     throw new Exception("unknown pickup type");
